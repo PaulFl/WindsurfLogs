a0b23681a36af84b45929aeef51024e5dca4d857
Paul Fleury <paul.fleuryp@gmail.com>
2022-02-11 19:46:50 +0100

    init commit

---
+++ WindsurfLogs/WindsurfLogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindsurfLogsApp.swift
+//  WindsurfLogs
+//
+//  Created by Paul Fleury on 11/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WindsurfLogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
